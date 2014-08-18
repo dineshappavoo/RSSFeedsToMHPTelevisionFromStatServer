b861f83ec1a311797bbc2e9ceaff1245c3d303db
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-08-18 17:38:19 -0500

    RSS feed statistic util

---
+++ src/com/rss/mhp/tv/RSSFeedStatUtil.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author b030513
+ *
+ */
+public class RSSFeedStatUtil {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * Method to parse the message string and get the call statistics
+	 * @param message
+	 * @return
+	 */
+	public static String getCallStat(String message)
+	{
+		String callStat="";
+		
+		message=message.trim();
+		callStat=message.substring(message.indexOf("STRING_VALUE [str] = \"")+22, message.indexOf("\" TM_SERVER [int] "));
+		return callStat;
+	}
+
+}
