10342ecd40d4410efa494e920504ff3ae833b119
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-09-11 01:14:29 -0500

    Updated XMLUpdate file

--- src/com/rss/mhp/tv/QueueXMLUpdate.java
+++ src/com/rss/mhp/tv/QueueXMLUpdate.java
@@ -10,26 +10,47 @@ import java.util.HashMap;
  * @author Dany
  *
  */
-public class QueueXMLUpdate {
+public class QueueXMLUpdate extends Thread{
 
 	/**
 	 * @param args
 	 */
+	Thread receiverThread = null;
+
 	public static void main(String[] args) {
 
 		new QueueXMLUpdate().getRSSFeedForQueues();
 	}
 	
+	public QueueXMLUpdate()
+	{
+		System.out.println("Thread|Dashboard:MessageReceiver() Initializing MessageReceiver Thread...");
+		receiverThread = new Thread(this, "RECEIVER");
+		receiverThread.start();
+	}
+	
+	// This method will receive all messages:
+		public void run() {
+			System.out.println("Thread|Dashboard::MessageReceiver:run() Starting MessageReceiver Thread...");
+			while (true) {
+					getRSSFeedForQueues();
+			}
+		}
+	
 	public void getRSSFeedForQueues()
 	{
-		HashMap<String,String> hQueueXMLFileMap=null;
-		String sXMLFileName="";
-		hQueueXMLFileMap=DMOperations.getXMlFileName();
-		
-		for(String queueName : hQueueXMLFileMap.keySet())
-		{
-			sXMLFileName=hQueueXMLFileMap.get(queueName);
-				StatMain.initilizeRequestProcessing(queueName, sXMLFileName);
+		try {
+			HashMap<String,String> hQueueXMLFileMap=null;
+			String sXMLFileName="";
+			hQueueXMLFileMap=DMOperations.getXMlFileName();
+			
+			for(String queueName : hQueueXMLFileMap.keySet())
+			{
+				sXMLFileName=hQueueXMLFileMap.get(queueName);
+					StatMain.initilizeRequestProcessing(queueName, sXMLFileName);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
